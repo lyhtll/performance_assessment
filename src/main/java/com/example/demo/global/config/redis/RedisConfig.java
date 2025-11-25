965cf5f8375fe0a7097ab4339deeb80e2e5b06a1
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-11-25 20:38:16 +0900

    fix: add default values and password support to Redis configuration

--- src/main/java/com/example/demo/global/config/redis/RedisConfig.java
+++ src/main/java/com/example/demo/global/config/redis/RedisConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.global.config.redis;
 
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableRedisRepositories
@@ -21,9 +23,16 @@ public class RedisConfig {
     @Value("${spring.data.redis.port:6379}")
     private int port;
 
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(host, port);
+        if (StringUtils.hasText(password)) {
+            factory.setPassword(password);
+        }
+        return factory;
     }
 
     @Bean
@@ -37,11 +46,15 @@ public class RedisConfig {
 
     @Bean
     public RedisClient redisClient() {
-        RedisURI redisUri = RedisURI.builder()
+        RedisURI.Builder builder = RedisURI.builder()
                 .withHost(host)
-                .withPort(port)
-                .build();
-        return RedisClient.create(redisUri);
+                .withPort(port);
+        
+        if (StringUtils.hasText(password)) {
+            builder.withPassword(password.toCharArray());
+        }
+        
+        return RedisClient.create(builder.build());
     }
 }
 
